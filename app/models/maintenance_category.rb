204795fd42a9a67e42643b1ede9a8bf5de0672c3
Rob <github email address>
2018-01-27 14:38:27 -0500

    set up associations

--- app/models/maintenance_category.rb
+++ app/models/maintenance_category.rb
@@ -1,2 +1,4 @@
 class MaintenanceCategory < ActiveRecord::Base
+  has_many :maintenance_logs
+  has_many :item_categories
 end
--- app/models/maintenance_item.rb
+++ app/models/maintenance_item.rb
@@ -1,2 +1,5 @@
 class MaintenanceItem < ActiveRecord::Base
+  belongs_to :user
+  has_many :maitnenance_logs
+  has_many :item_categories
 end
